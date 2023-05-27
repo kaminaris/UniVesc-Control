c01a534fd89b1d4f6a922d528cdd9c3869312b91
Pawel <wilkowski@gmail.com>
2023-05-27 07:01:52 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'UniVescDisplayControl',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
